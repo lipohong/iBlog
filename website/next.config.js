fd7ccff155c4f7626d617ec36d8122e24cc8c3c8
lipohong <lipohong@hotmail.com>
2020-07-31 18:11:58 +0800

    Update website, remove customized server.js

--- website/next.config.js
+++ website/next.config.js
@@ -1,11 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  publicRuntimeConfig: {
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-      ? process.env.LOCALE_SUBPATHS
-      : 'none',
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, './assets/css/styles')],
   }
--- website/plugins/i18n.js
+++ website/plugins/i18n.js
@@ -1,19 +1,11 @@
 const defaultNextI18Next = require('next-i18next').default
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
-
-const localeSubpathVariations = {
-  none: {},
-  foreign: {
-    zh: 'zh',
-  },
-  all: {
-    en: 'en',
-    zh: 'zh',
-  },
-}
 
 module.exports = new defaultNextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['zh'],
-  localeSubpaths: localeSubpathVariations[localeSubpaths]
+  defaultNS: ['common'],
+  localeSubpaths: {
+    en: 'en',
+    zh: 'zh',
+  }
 })
