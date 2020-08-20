0ee41ef31c92979758d673f1f716bda5a0667259
lipohong <lipohong@hotmail.com>
2020-08-20 14:02:02 +0800

    Update comment server, update port setting for dockering

--- server/comment/server.ts
+++ server/comment/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import globalVars from './models/globalVars';
 
-const PORT = globalVars.port || 8012;
+const PORT = globalVars.port || 8013;
 
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
