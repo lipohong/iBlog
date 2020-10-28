e05f0961aa5117251efe27a3747bf4f15a3cf7e9
lipohong <lipohong@hotmail.com>
2020-10-28 18:24:05 +0800

    Update blog server, change route name for getting most viewed and blog posters

--- server/blog/routes/blogRoute.ts
+++ server/blog/routes/blogRoute.ts
@@ -17,8 +17,8 @@ export class blogRoute {
     router.get('/myBlogs', auth(true), this.blogController.getMyBlogs);
     router.get('/user/:userId', auth(false), this.blogController.getUserBlogs);
     router.get('/user/:userId/amount', auth(false), this.blogController.getUserBlogsAmount);
-    router.get('/top5/viewedBlogs', auth(false), this.blogController.getTop5ViewedBlogs);
-    router.get('/top5/blogPosters', auth(false), this.blogController.getTop5BlogPosters);
+    router.get('/viewedBlogs/top5', auth(false), this.blogController.getTop5ViewedBlogs);
+    router.get('/blogPosters/top5', auth(false), this.blogController.getTop5BlogPosters);
     router.get('/:blogId', auth(false), this.blogController.getBlogById);
     router.post('/', auth(true), this.blogController.create);
     router.post('/blogs', auth(false), this.blogController.getBlogsByIds);
